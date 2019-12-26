6388b4efc1c49c418b3acc61d4632ebdfc5b9a85
khushi-dewara <46263012+khushi-dewara@users.noreply.github.com>
2019-12-26 10:30:03 +0530

    Create 15.sql

---
+++ 15.sql
@@ -0,0 +1,3 @@
+/*AIM:Find the min,max,sum, and average of the marks in a student marks table.*/
+Select min(marks) as minimum_marks , max(marks) as maximum_marks , sum(marks) as total_marks , avg(marks) as average_marks
+From student_marks ;
