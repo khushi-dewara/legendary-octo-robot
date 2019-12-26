2d5a7f380c2be8f8b1c663c0a54aa295226016c3
khushi-dewara <46263012+khushi-dewara@users.noreply.github.com>
2019-12-26 09:35:55 +0530

    Create 13EMI.py

---
+++ 13EMI.py
@@ -0,0 +1,12 @@
+#AIM:Compute EMIs for a loan using the numpy or scipy libraries.
+import numpy as np
+# assume annual interest of 7.5%
+def calc_interest(interest ,years , loan_value ):
+   annual_rate = interest/100.0
+   monthly_rate = annual_rate/12
+   number_month = years * 12
+   monthly_payment = abs(np.pmt(monthly_rate, number_month, loan_value))
+   sf1 = "Paying off a loan of ${:,} over {} years at"
+   sf2 = "{}% interest, your monthly payment will be ₹{:,.2f}"
+   print(sf1.format(loan_value, years))
+   print(sf2.format(interest, monthly_payment))
