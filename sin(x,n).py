1a1c95867ae771ff6e9fbb7b406a9d2339c34a20
khushi-dewara <46263012+khushi-dewara@users.noreply.github.com>
2019-12-26 10:03:25 +0530

    Create sin(x,n).py

---
+++ sin(x,n).py
@@ -0,0 +1,2 @@
+'''AIM:Write a python function sin(x,n) to calculate the value of sin(x) using its Taylor series expansion up to n terms .
+Compare the values of sin(x) for different values of n with the correct value.'''
