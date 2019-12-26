9196344c5315acff4a8843ffcf62920d5af9e2da
khushi-dewara <46263012+khushi-dewara@users.noreply.github.com>
2019-12-26 10:20:15 +0530

    Update 14e-mail.py

--- 14e-mail.py
+++ 14e-mail.py
@@ -1,1 +1,17 @@
 #AIM:Take a sample of 10 phishing e-mails and find the most common words.
+# from data set 
+from collections import Counter 
+
+#data_set = "phished emails"  
+def most_common_word(dataset , n=10):   
+    # split() returns list of all the words in the string 
+    split_it = data_set.split() 
+    
+    # Pass the split_it list to instance of Counter class. 
+    count = Counter(split_it) 
+    
+    # most_common() produces k frequently encountered 
+    # input values and their respective counts. 
+    most_occur = count.most_common(n) 
+    
+    print(most_occur)
