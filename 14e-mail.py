d001dc4d4d412bee46404fa00ee262c616c308b3
khushi-dewara <46263012+khushi-dewara@users.noreply.github.com>
2020-01-09 21:40:54 +0530

    Update 14e-mail.py

--- 14e-mail.py
+++ 14e-mail.py
@@ -3,14 +3,14 @@
 from collections import Counter 
 
 #data_set = "phished emails"  
-def most_common_word(dataset , n=10):   
+def most_common(dataset , n=10):   
     # split() returns list of all the words in the string 
     split_it = data_set.split() 
     
     # Pass the split_it list to instance of Counter class. 
     count = Counter(split_it) 
     
-    # most_common() produces k frequently encountered 
+    # most_common() produces  frequently encountered 
     # input values and their respective counts. 
     most_occur = count.most_common(n) 
     
